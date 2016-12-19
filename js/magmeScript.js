64228b447adcaf0ed0c41c9ae3beb71eddac5abc
MagmMe <marcin@magm.me>
2016-12-19 12:35:43 +0100

    Adding AOS Effect to website, editing website elements

--- js/magmeScript.js
+++ js/magmeScript.js
@@ -14,22 +14,52 @@ function animationHover(element, animation){
         });
 }
 
+function initMap() {
+        var uluru = {lat:49.8178201, lng:19.0363254};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+
+        options = $.extend({
+            scrollwheel: false,
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }, options);
+      }
+
+       $('#map').addClass('scrolloff'); // set the pointer events to none on doc ready
+        $('#map').on('click', function () {
+            $('#map').removeClass('scrolloff'); // set the pointer events true on click
+        });
+
+
+
 $(document).ready(function(){
     $('.magmeAnimation').each(function() {
         animationHover(this, 'pulse');
     });
+
+
+      $.cookieBar();
+
+            // AOS Effects Library
+
+      AOS.init({
+      offset: 200,
+      duration: 2000,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+
+
 });
-
-
-$(document).ready(function () {
-
-    $(".player").mb_YTPlayer();
-
-});
-
-
-
-
-
 
 
